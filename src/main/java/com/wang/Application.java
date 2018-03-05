9e15617a0056f9eddc78139be3060c0092886771
firstcapf <172128788@qq.com>
2018-03-05 15:31:14 +0800

    swagger2

--- src/main/java/com/wang/Application.java
+++ src/main/java/com/wang/Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
