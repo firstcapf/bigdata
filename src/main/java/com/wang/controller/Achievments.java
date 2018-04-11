3ac77e0069dc241828cd2da86154a844670ed8d3
sully <835916033@qq.com>
2018-04-11 18:30:22 +0800

    研究方向和研究成果跳转

---
+++ src/main/java/com/wang/controller/Achievments.java
@@ -0,0 +1,23 @@
+package com.wang.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+@EnableAutoConfiguration
+@RequestMapping("/achievments")
+
+public class Achievments {
+
+    @RequestMapping("/achievments")
+    public String achievments(HttpServletRequest request, Map<String,Object> model){
+
+        return  "achievments/achievments";
+        //返回的内容就是templetes下面文件的名称
+    }
+
+}
